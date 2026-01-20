035fa91a4127c4121c82e97ee8e748dbd060ecdc
TdubMorris <95392519+TdubMorris@users.noreply.github.com>
2026-01-19 23:49:23 -0600

    Added new page

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import { HomePage } from "./pages/HomePage";
 import "./styling/index.css"
 import { NotFound } from "./pages/NotFound";
 import RandomProject from "./pages/RandomProject";
+import { Secret } from "./pages/secret";
 
 function App() {
 
   return (
       <Routes>
         <Route path="" element={<HomePage/>} />
-         <Route path="/random" element={<RandomProject/>}/>
+        <Route path="/random" element={<RandomProject/>}/>
+        <Route path="/secret" element={<Secret/>}/>
         <Route path="*" element={<NotFound/>} />
       </Routes>
   )
---
+++ src/pages/Secret.jsx
@@ -0,0 +1,11 @@
+export const Secret = () => {
+
+return (
+  <iframe className="w-full h-screen" 
+  src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=Vl2UVnWYMuWYQP67&autoplay=1&mute=1" 
+  allow="autoplay; encrypted-media"
+  allowFullScreen
+  ></iframe>
+)
+
+}
