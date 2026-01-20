a817449b75957eaa452427de976681658758b9e2
TdubMorris <95392519+TdubMorris@users.noreply.github.com>
2026-01-19 21:56:53 -0600

    Updated 404 page.

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,9 +3,10 @@ import { Background } from "../components/home/Background"
 export const NotFound = () => {
   return (<>
   <Background/>
-  <div className="h-screen w-screen flex justify-center items-center flex-col space-y-32">
+  <div className="h-screen w-screen flex justify-center items-center flex-col">
       <h1 className="text-9xl font-bold italic">404</h1>
-      <p className="text-white/10">Let the mind wander.</p>
+      <p className="text-white/10 mb-[5%]">Let the mind wander.</p>
+      <a className="font-bold text-blue-600 hover:underline mx-2 text-nowrap" href="/" target="_self">Return to home page</a>
   </div>
   </>)
 }
